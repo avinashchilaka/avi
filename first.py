ae318809a4d3b2b98534827e83c392943380f4e9
Avi <avinash.chilaka@gmail.com>
2019-03-28 22:45:34 -0700

    i made changes f=30

--- first.py
+++ first.py
@@ -4,8 +4,10 @@ c=400
 d=100
 e=500
 f=30
-z=a+b+c+d
+z=a+b+c+d+e+f
 print "The total sum is",z
 print "variable d added"
 print "Variable e added"
 print "Variable f is equal to 30"
+print "You are coming from test branch"
+print "Your are very new in this branch"
